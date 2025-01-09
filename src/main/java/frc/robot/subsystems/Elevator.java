3fa7b8b2e2990d242430bc7b052806eeccdc7243
laser 3284 <laser3284@camdentonschools.org>
2025-01-09 15:15:26 -0600

    elev

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,14 +1,41 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.controller.PIDController;
+
+import frc.robot.Constants;
+
 public class Elevator {
-    
+
+    double kP;
+    double kI;
+    double kD;
+
+    public enum PIDmodes {
+        position,
+        velocity,
+        with_algae,
+        without_algae
+    }
+
+    TalonFX motor1 = new TalonFX(Constants.ElevatorConstants.E1_ID);
+    TalonFX motor2 = new TalonFX(Constants.ElevatorConstants.E2_ID);
+
+    PIDController pid = new PIDController(kP, kI, kD);
+
+    public Elevator() {}
+
+    public void set_speed(double s1, double s2) {
+        motor1.set(s1);
+        motor2.set(s2);
+    }
+
+    public void stop() {
+        motor1.set(0);
+        motor2.set(0);
+    }
     // gonna need a trapezoidal pid
-
-    //enum of pid modes? need to look more into that
-
-    //no decision on what type of motors and how many yet
-
-    //prob through-bores on them though... most likely revs
 
     // set points and goals and such
 }
