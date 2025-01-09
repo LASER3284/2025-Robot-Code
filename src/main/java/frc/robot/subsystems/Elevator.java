56bf73e8ce2609b284048a06b173947cbc4355fd
laser 3284 <laser3284@camdentonschools.org>
2025-01-09 11:45:09 -0600

    yeah

---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+public class Elevator {
+    
+    // gonna need a trapezoidal pid
+
+    //enum of pid modes? need to look more into that
+
+    //no decision on what type of motors and how many yet
+
+    //prob through-bores on them though... most likely revs
+
+    // set points and goals and such
+}
